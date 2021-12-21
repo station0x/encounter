6db3e5d1e9226837a17b315c916186fab9a2a1d1
captinisaac@gmail.com <captinisaac@gmail.com>
2021-12-21 12:23:49 +0200

    fix debounce

--- src/store/index.js
+++ src/store/index.js
@@ -140,7 +140,7 @@ export default new Vuex.Store({
                     break
                 }
                 if(axiosQueue.getQueueLength() === 0 && axiosQueue.getPendingLength() <= 1) {
-                    if(state.matchState.playerIs === matchDoc.playerTurn) { // in case there's a pending end turn
+                    if(state.matchState.playerIs !== matchDoc.playerTurn) { 
                         debouncedMatchState(matchDoc)
                     } else {
                         commit("setMatchState", matchDoc)
