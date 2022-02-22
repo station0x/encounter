9d6431708c6e3422e413d069b60cf0f77b5b1f71
captinisaac@gmail.com <captinisaac@gmail.com>
2022-02-22 16:50:44 +0200

    Rewards, claiming, Inventory and Game Guide Added

--- contracts/scripts/deployTestnet.sh
+++ contracts/scripts/deployTestnet.sh
@@ -1,6 +1,9 @@
 export ETH_RPC_URL=https://rpcapi-tracing.testnet.fantom.network
 export ETHERSCAN_API_KEY=76GP9TIQJXSJKVAG2X7NMIFT1319KD11X9
 # deploy ores
+forge create ERC20 --interactive --legacy --chain fantom --constructor-args Halfinium HALF
+forge create ERC20 --interactive --legacy --chain fantom --constructor-args Sangius SAN
+
 
 # Halfinium
-forge create OresMinter --interactive --legacy --chain fantom-testnet --constructor-args 0xC6eEeeA35daB4C21388641BfF863225b94c729d2 0x79a3761e0584df2485cd07eb8969de8827298fa1
+# forge create OresMinter --interactive --legacy --chain fantom --constructor-args 0x3dC1ADE4aF16CC4A3606991b2654e7629734A200 0x81FD43e82aa2D0aE59f59Ff3B999a0a6edE663E8
