9cf91f60f7aae9c770995e43acb0f3fa4b443113
captinisaac@gmail.com <captinisaac@gmail.com>
2022-02-27 04:30:49 +0200

    [fix] picking logging and rpc testnet endpoint

--- api/matchmaker/joinMatchmaking.js
+++ api/matchmaker/joinMatchmaking.js
@@ -1,6 +1,6 @@
 // TO-DO: Board initialization needs to be modular and imported from one file to here and create match by inviation
 "use strict";
-const CONSTANTS = require('../../constants.json')
+const CONSTANTS = require('../../constants')
 // Import the dependency.
 const clientPromise = require('../../api-utils/mongodb-client');
 const getAddress = require('../../api-utils/getAddress');
@@ -32,7 +32,7 @@ module.exports = async (req, res) => {
         await matchmakingQueue.deleteOne({address: enemyAddress})
 
        const matches = db.collection("matches")
-       const board = CONSTANTS.board.map(row => row.map(col => {
+       const board = CONSTANTS.initialBoardState.map(row => row.map(col => {
            if(!col.type) {
                return col
            }
