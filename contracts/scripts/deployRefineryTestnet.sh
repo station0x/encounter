e42693f1a25d5cb454fa054a681667b68169781f
captinisaac@gmail.com <captinisaac@gmail.com>
2022-03-30 06:41:00 +0200

    moved to hardhat

--- contracts/scripts/deployRefineryTestnet.sh
+++
@@ -1,2 +0,0 @@
-
-forge create RefineryDeployer --private-key 1736d7ef2a410284e4128f0e98ca9a3643fedd46810667582aca6716bc390cd2 --legacy --rpc-url https://rpcapi-tracing.testnet.fantom.network/ --constructor-args 0x6BDD6Bb68Ec6927F56749b46746F0AFA7CdA9F3c --extra-output-files metadata
--- contracts/src/refinery/test/gas.t.sol
+++
@@ -1,8 +0,0 @@
-pragma solidity 0.8.10;
-import "../RefineryDeployer.sol";
-
-contract Gas {
-    function testGas() public {
-        new RefineryDeployer(address(this));
-    }
-}
